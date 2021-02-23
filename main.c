368c1bc292063fb209a19eec77eb58f210245cb1
Abhijeet Chitagubbi <77390129+abhijeetlc@users.noreply.github.com>
2021-02-24 00:08:24 +0530

    Update main.c for modularity

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
+#include"test.h"
 
 //enumerate major and minor colors
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
@@ -63,50 +64,11 @@ int GetPairNumberFromColor(const ColorPair* colorPair)
     return (colorPair->majorColor * numberOfMinorColors + colorPair->minorColor + 1);
 }
 
-//test to check number pair matches color pair
-void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor)
-{
-    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
-    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
-    ColorPairToString(&colorPair, colorPairNames);
-    printf("Got pair %s\n", colorPairNames);
-    assert(colorPair.majorColor == expectedMajor);
-    assert(colorPair.minorColor == expectedMinor);
-}
 
-//test to check color pair matches number pair
-void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber)
-{
-    ColorPair colorPair;
-    colorPair.majorColor = major;
-    colorPair.minorColor = minor;
-    int pairNumber = GetPairNumberFromColor(&colorPair);
-    printf("Got pair number %d\n", pairNumber);
-    assert(pairNumber == expectedPairNumber);
-}
-
-// function to print the color pair - number pair as a reference manual
-void display_reference_manual()
-{
-	ColorPair Pair;
-	int numberpair = 1;
-	char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
-	printf(" PAIRNUMBER \t\t\t\t\t MAJORCOLOR-MINORCOLOR \n");
-	while (numberpair <= MAX_COLORPAIR_NAME_CHARS)
-	{
-		ColorPair Pair = GetColorFromPairNumber(numberpair);
-    	ColorPairToString(&Pair, colorPairNames);
-		printf(" %d \t\t\t\t\t %s\n ",numberpair,colorPairNames);
-		numberpair++;
-		
-	}
-	
-}
 int main() 
 {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
-
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
 	display_reference_manual(); // print reference manual of number pair - color pair
